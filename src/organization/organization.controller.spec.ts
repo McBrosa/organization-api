580b5f8d62742478aa5b099ddb901215eebea523
McBrosa <rosan@vt.edu>
2020-11-22 23:19:10 -0500

    fix UTs

--- src/organization/organization.controller.spec.ts
+++ src/organization/organization.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
+import { Organization } from './organization.entity';
 
 describe('OrganizationController', () => {
   let controller: OrganizationController;
@@ -8,7 +11,14 @@ describe('OrganizationController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrganizationController],
-      providers: [OrganizationService],
+      providers: [
+        OrganizationService,
+        // Provide your mock instead of the actual repository
+        {
+          provide: getRepositoryToken(Organization),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<OrganizationController>(OrganizationController);
--- src/organization/organization.service.spec.ts
+++ src/organization/organization.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { OrganizationService } from './organization.service';
+import { Organization } from './organization.entity';
 
 describe('OrganizationService', () => {
   let service: OrganizationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrganizationService],
+      providers: [
+        OrganizationService,
+        // Provide your mock instead of the actual repository
+        {
+          provide: getRepositoryToken(Organization),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<OrganizationService>(OrganizationService);
