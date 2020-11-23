aaa27b8ed38cb650960c62927d6d127653c796f4
McBrosa <rosan@vt.edu>
2020-11-22 21:17:41 -0500

    setup for heroku

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('Organization')
@@ -17,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(+configService.get<number>('APP_PORT'));
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
